423518903a39562fdcf445285fd7f8ed5b71e025
KristianNikolov07 <kris.nikolov07@gmail.com>
2026-03-27 01:39:09 +0200

    The day end works now

--- Scripts/Global/Global.gd
+++ Scripts/Global/Global.gd
@@ -6,3 +6,15 @@ func get_stats() -> Node:
 
 func get_player() -> Player:
 	return get_tree().current_scene.find_child("Player")
+
+
+func get_billing_screen() -> Node:
+	return get_tree().current_scene.find_child("Billing")
+
+
+func get_day_count() -> Node:
+	return get_tree().current_scene.find_child("DayCount")
+
+
+func get_start_pos() -> Marker2D:
+	return get_tree().current_scene.find_child("StartPos")
